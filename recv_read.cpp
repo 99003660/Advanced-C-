9eabdf93fb524d44ecc9b7432849f3c187b61d3e
99003660 <78850394+99003660@users.noreply.github.com>
2021-04-10 18:15:37 +0530

    Create recv_read.cpp

---
+++ recv_read.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<fstream>
+using namespace std;
+void read()
+{
+    ifstream fin;
+    string line;
+    // Open an existing file
+    fin.open("Book1.csv");
+     while(!fin.eof()){
+                fin>>line;
+            cout<<line<<" ";
+            }
+    }
+int main()
+{
+    read();
+    return 0;
+}
